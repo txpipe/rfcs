10517600739442ffaa7703bef0bb34adbd81cce9
Santiago Carmuega <santiago@carmuega.me>
2022-12-17 19:05:29 -0300

    refactor: Migrate to nextra for rendering

---
+++ theme.config.jsx
@@ -0,0 +1,18 @@
+export default {
+    logo: <span>TxPipe RFC</span>,
+    project: {
+        link: "https://github.com/txpipe/rfcs",
+    },
+    chat: {
+        link: "https://discord.gg/Vc3x8N9nz2",
+    },
+    docsRepositoryBase: "https://github.com/txpipe/rfcs/blob/main",
+    useNextSeoProps() {
+        return {
+            titleTemplate: "%s – TxPipe RFCs",
+        };
+    },
+    footer: {
+        text: "TxPipe RFCs",
+    },
+};
