bea34e09468d906894581562cbf4448359b1b9bb
federico s weill <federico.weill@line64.com>
2024-06-12 11:25:40 -0300

    grants inclusion

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,22 +1,45 @@
 export default {
-    logo: <span>TxPipe RFC</span>,
+    logo: <span>TxPipe Grants</span>,
     project: {
-        link: "https://github.com/txpipe/rfcs",
+        link: "https://github.com/txpipe/",
     },
     chat: {
         link: "https://discord.gg/Vc3x8N9nz2",
     },
     docsRepositoryBase: "https://github.com/txpipe/rfcs/blob/main",
+    sidebar: {
+        items: [
+            {
+                title: 'Grants',
+                items: [
+                    {
+                        title: 'Catalyst',
+                        items: [
+                            { title: 'Fund-9', path: '/Grants/Catalyst/Fund-9' },
+                            { title: 'Fund-10', path: '/Grants/Catalyst/Fund-10' },
+                            { title: 'Fund-11', path: '/Grants/Catalyst/Fund-11' },
+                        ],
+                    },
+                    {
+                        title: 'Deep-Funding',
+                        items: [
+                            { title: 'Round-2', path: '/Grants/Deep-Funding/Round-2' },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
     useNextSeoProps() {
         return {
-            titleTemplate: "%s – TxPipe RFCs",
-            description: "TxPipe's Request for Comments repository",
-            canonical: "https://rfcs.txpipe.io",
-            siteName: "TxPipe RFCs",
+            titleTemplate: "%s – TxPipe Grants",
+            description: "TxPipe's Grants",
+            canonical: "https://grants.txpipe.io",
+            siteName: "TxPipe Grants",
             openGraph: {
-                url: "https://rfcs.txpipe.io",
-                title: "TxPipe RFCs",
-                description: "TxPipe's Request for Comments repository",
+                url: "https://grants.txpipe.io",
+                title: "TxPipe Grants",
+                description: "TxPipe's Grants Report",
             },
             twitter: {
                 handle: "@txpipe_tools",
@@ -24,6 +47,7 @@ export default {
         };
     },
     footer: {
-        text: "TxPipe RFCs",
+        text: "TxPipe Grants",
     },
+    
 };
